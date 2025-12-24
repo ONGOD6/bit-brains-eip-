2c0ac69662c52da1acb72a76e0b10c201f1a841d
ONGOD <115197964+ONGOD6@users.noreply.github.com>
2025-12-24 06:53:11 -0500

    Create layout.tsx
    init: add Next.js app router layout

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "BitBrains",
+  description: "BitBrains Protocol",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
